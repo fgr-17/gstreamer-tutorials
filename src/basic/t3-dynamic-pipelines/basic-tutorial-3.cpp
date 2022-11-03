0efc8008b4b11aa915dbe1dbd57e1a04ca5255bb
Federico Roux <rouxfederico@gmail.com>
2022-11-03 07:13:10 -0300

    video not working

--- src/basic/t3-dynamic-pipelines/basic-tutorial-3.cpp
+++ src/basic/t3-dynamic-pipelines/basic-tutorial-3.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include <gst/gst.h>
+
+void print_all_structs_from_caps (GstCaps * caps);
 
 /* Structure to contain all our information, so we can pass it to callbacks */
 typedef struct _CustomData {
@@ -45,9 +48,18 @@ int main(int argc, char *argv[]) {
 
   /* Build the pipeline. Note that we are NOT linking the source at this
    * point. We will do it later. */
-  gst_bin_add_many (GST_BIN (data.pipeline), data.source, data.convert, data.resample, data.sink, NULL);
+  gst_bin_add_many (GST_BIN (data.pipeline), data.source, data.convert, data.resample, data.sink, data.videoconvert, data.videosink, NULL);
+  
+  /* linking audio elements */
   if (!gst_element_link_many (data.convert, data.resample, data.sink, NULL)) {
-    g_printerr ("Elements could not be linked.\n");
+    g_printerr ("Audio elements could not be linked.\n");
+    gst_object_unref (data.pipeline);
+    return -1;
+  }
+
+  /* linking video elements */
+  if (!gst_element_link (data.videoconvert, data.videosink)) {
+    g_printerr ("Video elements could not be linked.\n");
     gst_object_unref (data.pipeline);
     return -1;
   }
@@ -70,7 +82,7 @@ int main(int argc, char *argv[]) {
   bus = gst_element_get_bus (data.pipeline);
   do {
     msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
-        GST_MESSAGE_STATE_CHANGED | GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+        static_cast<GstMessageType>(GST_MESSAGE_STATE_CHANGED | GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
 
     /* Parse message */
     if (msg != NULL) {
@@ -118,6 +130,8 @@ int main(int argc, char *argv[]) {
 /* This function will be called by the pad-added signal */
 static void pad_added_handler (GstElement *src, GstPad *new_pad, CustomData *data) {
   GstPad *sink_pad = gst_element_get_static_pad (data->convert, "sink");
+  GstPad *videosink_pad = gst_element_get_static_pad (data->videoconvert, "videosink");
+
   GstPadLinkReturn ret;
   GstCaps *new_pad_caps = NULL;
   GstStructure *new_pad_struct = NULL;
@@ -128,31 +142,52 @@ static void pad_added_handler (GstElement *src, GstPad *new_pad, CustomData *dat
   /* If our converter is already linked, we have nothing to do here */
   if (gst_pad_is_linked (sink_pad)) {
     g_print ("We are already linked. Ignoring.\n");
-    goto exit;
+    /* Unreference the new pad's caps, if we got them */
+
+    /* Unreference the sink pad */
+    gst_object_unref (sink_pad);
+    gst_object_unref (videosink_pad);
+    return;
   }
 
   /* Check the new pad's type */
   new_pad_caps = gst_pad_get_current_caps (new_pad);
   new_pad_struct = gst_caps_get_structure (new_pad_caps, 0);
   new_pad_type = gst_structure_get_name (new_pad_struct);
-  if (!g_str_has_prefix (new_pad_type, "audio/x-raw")) {
-    g_print ("It has type '%s' which is not raw audio. Ignoring.\n", new_pad_type);
-    goto exit;
+
+  if(g_str_has_prefix (new_pad_type, "audio/x-raw")) {
+    /* Attempt the link */
+    ret = gst_pad_link (new_pad, sink_pad);
+  } 
+  else if(g_str_has_prefix (new_pad_type, "video/x-raw")) {
+    /* Attempt the link */
+    ret = gst_pad_link (new_pad, videosink_pad);
+  }
+  else {
+    g_print ("It has type '%s' which is not raw audio or raw video. Ignoring.\n", new_pad_type);
+    /* Unreference the new pad's caps, if we got them */
+    if (new_pad_caps != NULL)
+      gst_caps_unref (new_pad_caps);
+
+    /* Unreference the sink pad */
+    gst_object_unref (sink_pad);
+    gst_object_unref (videosink_pad);
+    return;  
   }
 
-  /* Attempt the link */
-  ret = gst_pad_link (new_pad, sink_pad);
   if (GST_PAD_LINK_FAILED (ret)) {
     g_print ("Type is '%s' but link failed.\n", new_pad_type);
   } else {
     g_print ("Link succeeded (type '%s').\n", new_pad_type);
+    return;
   }
 
-exit:
   /* Unreference the new pad's caps, if we got them */
   if (new_pad_caps != NULL)
     gst_caps_unref (new_pad_caps);
 
   /* Unreference the sink pad */
   gst_object_unref (sink_pad);
+  gst_object_unref (videosink_pad);
+  return;
 }
